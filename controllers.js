74898dd1a03b091cfa34b5b6f2681281b3bef10d
saptarshi <saptarshidutta31@yahoo.co.in>
2017-05-02 19:30:51 +0530

    using service to fetch weather data from network

--- controllers.js
+++ controllers.js
@@ -6,14 +6,13 @@ weatherApp.controller('homeController',['$scope','cityService','$log',function($
 	});
 }]);
 
-weatherApp.controller('forecastController',['$scope','cityService','$resource','$log','$routeParams',
-	function($scope,cityService,$resource,$log,$routeParams){
+weatherApp.controller('forecastController',['$scope','cityService','weatherService','$resource','$log','$routeParams',
+	function($scope,cityService,weatherService,$resource,$log,$routeParams){
 	
 
 	$scope.city = cityService.city;
 	$scope.days = $routeParams.days || '2';
-	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily",{get:{method:"JSONP"}});
-	$scope.weatherResult = $scope.weatherAPI.get({q:$scope.city,cnt:$scope.days,APPID:"3b58f211784eb02e52d9c93a76e18f90"});
+	$scope.weatherResult = weatherService.getWeatherResult($scope.city,$scope.days);
 	$scope.tempConvertor = function(degK){
 		return Math.round(degK - 273);
 	}
--- services.js
+++ services.js
@@ -2,3 +2,9 @@
 weatherApp.service('cityService',function(){
 	this.city = 'Mysore';
 })
+weatherApp.service('weatherService',['$resource',function($resource){
+	this.getWeatherResult = function(city,days){
+		var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily",{get:{method:"JSONP"}});
+		return weatherAPI.get({q:city,cnt:days,APPID:"3b58f211784eb02e52d9c93a76e18f90"});
+	}
+}]);
