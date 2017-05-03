4a7556b883289de85d1e00d1b0d3cb6d152230fb
saptarshi <saptarshidutta31@yahoo.co.in>
2017-05-03 18:58:46 +0530

    adding form submit to city search

--- controllers.js
+++ controllers.js
@@ -1,9 +1,12 @@
 // controllers
-weatherApp.controller('homeController',['$scope','cityService','$log',function($scope,cityService,$log){
+weatherApp.controller('homeController',['$scope','cityService','$log','$location',function($scope,cityService,$log,$location){
 	$scope.city = cityService.city;
 	$scope.$watch('city',function(){
 		cityService.city = $scope.city;
 	});
+	$scope.submit = function(){
+		$location.path("/forecast");
+	}
 }]);
 
 weatherApp.controller('forecastController',['$scope','cityService','weatherService','$resource','$log','$routeParams',
